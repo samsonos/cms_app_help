78f0ed6de1ea58b48e5fcc05424c75f0bde78258
Vitaly Egorov <egorov@samsonos.com>
2015-04-22 08:14:39 +0300

    Added events for rendering content and submenu

--- src/Application.php
+++ src/Application.php
@@ -19,24 +19,31 @@ class Application extends \samsoncms\Application
     /** Identifier */
     protected $id = 'help';
 
+    /**
+     * Event handler for rendering main menu sub menu
+     * @param string $html Main menu HTML
+     * @param string $subMenu Sub menu HTML
+     */
+    public function subMenuHandler(&$html, &$subMenu)
+    {
+        $subMenu = '<li><a href="#top">'.t('В начало', true).'</a></li>';
+
+        // Fire event when application help is rendered
+        \samsonphp\event\Event::fire('help.submenu.rendered', array(&$subMenu));
+    }
+
     /** Universal controller action */
     public function __handler()
     {
-        $html = '';
+        // Render menu
+        \samsonphp\event\Event::subscribe('template.menu.rendered', array($this, 'subMenuHandler'));
 
-        // TODO: This approach must be removed in favor of events
-        // Iterate all loaded applications
-        foreach (self::$loaded as $app) {
-            // Try to find application help view
-            if ($app->findView('help/index')) {
-                // Render help page
-                $html .= $app->view('help/index')->output();
-            }
-        }
+        $html = '';
 
         // Fire event when application help is rendered
         \samsonphp\event\Event::fire('help.content.rendered', array(&$html));
 
+        // Prepare view
         $this->view('index')
             ->title(t($this->name, true))
         ->set('content', $html);
